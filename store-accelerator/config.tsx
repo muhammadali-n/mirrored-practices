41cdd2fe249a079f6591de2c175db77be8788470
neethukp <neethu.kp@mozanta.com>
2024-02-05 14:58:40 +0530

    integration layer

---
+++ store-accelerator/config.tsx
@@ -0,0 +1,44 @@
+// export const configurations = {
+// shopify:{
+//     apiEndpoint: 'https://mozanta-theme-store.myshopify.com/api/graphql.json',
+//     storefrontAccessToken:"3a582815a77119eacfdd9d13abaa9e25",
+       
+// }
+//   };
+  
+//   export const getConfig = () => {
+
+//     const integrationType = process.env.INTEGRATION_TYPE || 'shopify'; 
+//     const config = configurations[integrationType];
+  
+//     if (!config) {
+//       throw new Error(`Invalid integration type: ${integrationType}`);
+//     }
+  
+//     return { integrationType, ...config };
+//   };
+  
+export interface Configurations {
+  shopify: {
+    apiEndpoint: string;
+    storefrontAccessToken: string;
+  };
+}
+
+export const configurations: Configurations = {
+  shopify: {
+    apiEndpoint: 'https://mozanta-theme-store.myshopify.com/api/graphql.json',
+    storefrontAccessToken: '3a582815a77119eacfdd9d13abaa9e25',
+  },
+};
+
+export const getConfig = () => {
+  const integrationType: string = process.env.INTEGRATION_TYPE || 'shopify';
+  const config = configurations[integrationType];
+
+  if (!config) {
+    throw new Error(`Invalid integration type: ${integrationType}`);
+  }
+
+  return { integrationType, ...config };
+};
--- store-accelerator/integrations/shopify/shopify-transformer.js
+++
@@ -1,9 +0,0 @@
-//handling non generic transformations
-const productTransformation = (data) => {
-  const transformedData = data.map((data) => ({
-    objectID: data.id
-  }));
-  return transformedData;
-};
-
-export { productTransformation };
