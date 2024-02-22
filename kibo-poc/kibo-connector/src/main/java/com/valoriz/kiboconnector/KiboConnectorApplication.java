c44048641cbd18d159349000c19bf2897f8d4fee
Mohamed-Saajid-KM <mohamed.saajid@mozanta.com>
2024-02-22 14:46:00 +0530

    kibo-connector project created

---
+++ kibo-poc/kibo-connector/src/main/java/com/valoriz/kiboconnector/KiboConnectorApplication.java
@@ -0,0 +1,13 @@
+package com.valoriz.kiboconnector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KiboConnectorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KiboConnectorApplication.class, args);
+	}
+
+}
