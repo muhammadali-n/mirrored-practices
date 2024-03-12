16c04f454fdb48d6f9a1949c8651d39f2a8230f2
Mohamed-Saajid-KM <mohamed.saajid@mozanta.com>
2024-03-12 16:26:38 +0530

    refactorings

--- kibo-poc/kibo-connector/src/main/java/com/valoriz/kiboconnector/scheduler/PIMScheduler.java
+++ kibo-poc/kibo-connector/src/main/java/com/valoriz/kiboconnector/scheduler/PIMScheduler.java
@@ -31,55 +31,55 @@ public class PIMScheduler {
 
     Logger logger = Logger.getLogger(PIMScheduler.class.getName());
 
-////     Runs in every 5 mins
-//    @Scheduled(fixedDelay = 300000)
-//    public void exportProductCatalogInformation() {
-//        logger.info("starting PIMScheduler to pick and process xml files");
-//        try {
-//            File folder = new File("catalog_xml_files");
-//            logger.info("folder: " + folder.getAbsolutePath());
-//            if (folder.exists() && folder.isDirectory()) {
-//                File[] files = folder.listFiles((dir, name) -> name.endsWith(".xml"));
-//                logger.info("xml files in the given path: " + folder);
-//                if (files != null) {
-//                    for (File file : files) {
-//                        logger.info("calling service method to process the xml file: " + file.toString());
-//                        pimService.processCatalogXmlFile(file);
-//                    }
-//                }
-//            } else {
-//                logger.severe("Invalid xml folder path");
-//            }
-//        }
-//        catch (Exception ex){
-//            logger.severe("Exception occurred while running PIMScheduler, Exception: " + ex.getMessage());
-//        }
-//    }
-//
-//    // Runs in every 5 mins
-//    @Scheduled(fixedDelay = 300000)
-//    public void exportProductPriceInformation() {
-//        logger.info("starting PIMScheduler to pick and process xml files");
-//        try {
-//            File folder = new File("price_xml_files");
-//            logger.info("folder: " + folder.getAbsolutePath());
-//            if (folder.exists() && folder.isDirectory()) {
-//                File[] files = folder.listFiles((dir, name) -> name.endsWith(".xml"));
-//                logger.info("xml files in the given path: " + folder);
-//                if (files != null) {
-//                    for (File file : files) {
-//                        logger.info("calling service method to process the xml file: " + file.toString());
-//                        pimService.processPriceXmlFile(file);
-//                    }
-//                }
-//            } else {
-//                logger.severe("Invalid xml folder path");
-//            }
-//        }
-//        catch (Exception ex){
-//            logger.severe("Exception occurred while running PIMScheduler, Exception: " + ex.getMessage());
-//        }
-//    }
+//     Runs in every 5 mins
+    @Scheduled(fixedDelay = 300000)
+    public void exportProductCatalogInformation() {
+        logger.info("starting PIMScheduler to pick and process xml files");
+        try {
+            File folder = new File("catalog_xml_files");
+            logger.info("folder: " + folder.getAbsolutePath());
+            if (folder.exists() && folder.isDirectory()) {
+                File[] files = folder.listFiles((dir, name) -> name.endsWith(".xml"));
+                logger.info("xml files in the given path: " + folder);
+                if (files != null) {
+                    for (File file : files) {
+                        logger.info("calling service method to process the xml file: " + file.toString());
+                        pimService.processCatalogXmlFile(file);
+                    }
+                }
+            } else {
+                logger.severe("Invalid xml folder path");
+            }
+        }
+        catch (Exception ex){
+            logger.severe("Exception occurred while running PIMScheduler, Exception: " + ex.getMessage());
+        }
+    }
+
+    // Runs in every 5 mins
+    @Scheduled(fixedDelay = 300000)
+    public void exportProductPriceInformation() {
+        logger.info("starting PIMScheduler to pick and process xml files");
+        try {
+            File folder = new File("price_xml_files");
+            logger.info("folder: " + folder.getAbsolutePath());
+            if (folder.exists() && folder.isDirectory()) {
+                File[] files = folder.listFiles((dir, name) -> name.endsWith(".xml"));
+                logger.info("xml files in the given path: " + folder);
+                if (files != null) {
+                    for (File file : files) {
+                        logger.info("calling service method to process the xml file: " + file.toString());
+                        pimService.processPriceXmlFile(file);
+                    }
+                }
+            } else {
+                logger.severe("Invalid xml folder path");
+            }
+        }
+        catch (Exception ex){
+            logger.severe("Exception occurred while running PIMScheduler, Exception: " + ex.getMessage());
+        }
+    }
 
     // Runs in every 5 mins
     @Scheduled(fixedDelay = 300000)
